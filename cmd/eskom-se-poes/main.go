75529e26d10079c644835ab65e3040554ba3c56f
Darth-Tenebros <yolisapingilili2@gmail.com>
2024-01-27 17:59:02 +0200

    got the tui working

--- cmd/eskom-se-poes/main.go
+++ cmd/eskom-se-poes/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"eskom-se-poes/internal/ui"
 	"eskom-se-poes/internal/utils"
 	"fmt"
+	"github.com/charmbracelet/bubbles/table"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 	"io"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -16,9 +21,43 @@ func main() {
 		fmt.Print(err)
 	}
 
+	var rows []table.Row
 	for _, outage := range schedule.Times {
-		fmt.Println(outage)
+		rows = append(rows, outage.OutageToSlice())
 	}
+
+	columns := []table.Column{
+		{Title: "Stage", Width: 5},
+		{Title: "Area Name", Width: 30},
+		{Title: "Start", Width: 30},
+		{Title: "Finish", Width: 30},
+	}
+
+	t := table.New(
+		table.WithColumns(columns),
+		table.WithRows(rows),
+		table.WithFocused(true),
+		table.WithHeight(7),
+	)
+
+	s := table.DefaultStyles()
+	s.Header = s.Header.
+		BorderStyle(lipgloss.NormalBorder()).
+		BorderForeground(lipgloss.Color("240")).
+		BorderBottom(true).
+		Bold(false)
+	s.Selected = s.Selected.
+		Foreground(lipgloss.Color("229")).
+		Background(lipgloss.Color("57")).
+		Bold(false)
+	t.SetStyles(s)
+
+	m := ui.TableModel{t}
+	if _, err := tea.NewProgram(m).Run(); err != nil {
+		fmt.Println("Error running program:", err)
+		os.Exit(1)
+	}
+
 }
 
 func getSchedule(link, area string) (*utils.Schedule, error) {
