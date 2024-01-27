6982fe4a2d51a09b6644f298047b4be28c328099
Darth-Tenebros <yolisapingilili2@gmail.com>
2024-01-27 16:37:42 +0200

    impl Stringer

--- internal/models/outage.go
+++ internal/models/outage.go
@@ -1,9 +1,16 @@
 package models
 
+import "fmt"
+
 type Outage struct {
 	AreaName string `json:"area_name"`
 	Finish   string `json:"finsh"`
 	Source   string `json:"source"`
 	Stage    int    `json:"stage"`
 	Start    string `json:"start"`
+}
+
+func (o Outage) String() string {
+	return fmt.Sprintf("AreaName: %s\nStart: %s\nFinish: %s\nStage: %d\n",
+		o.AreaName, o.Start, o.Finish, o.Stage)
 }
