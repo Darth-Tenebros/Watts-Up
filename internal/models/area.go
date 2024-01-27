2b37a2a83a738c41912309741ea8877328e42221
Darth-Tenebros <yolisapingilili2@gmail.com>
2024-01-27 13:56:54 +0200

    area model

--- internal/models/area.go
+++ internal/models/area.go
@@ -1,1 +1,9 @@
 package models
+
+type Area struct {
+	AreaName string `json:"area_name"`
+	Finish   string `json:"fnish"`
+	Source   string `json:"source"`
+	Stage    int    `json:"stage"`
+	Start    string `json:"start"`
+}
