a5eefcbf929bd6571e620a45e72411ab2f98d7ef
Darth-Tenebros <yolisapingilili2@gmail.com>
2024-01-27 14:36:28 +0200

    UnmarshalResponse func

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -3,10 +3,31 @@ package utils
 import (
 	"encoding/json"
 	"eskom-se-poes/internal/models"
+	"fmt"
 )
 
 type Schedule struct {
 	times []models.Outage
+}
+
+func (sch *Schedule) UnmarshalResponse(data []byte) error {
+	if len(data) == 0 {
+		return fmt.Errorf("no data to umarshal")
+	}
+
+	switch data[0] {
+	case '{':
+		return sch.unmarshalSingleItemResponse(data)
+	case '[':
+		return sch.unmarshalManyItemsResponse(data)
+	}
+
+	//jic
+	err := sch.unmarshalManyItemsResponse(data)
+	if err != nil {
+		return sch.unmarshalSingleItemResponse(data)
+	}
+	return nil
 }
 
 func (sch *Schedule) unmarshalSingleItemResponse(data []byte) error {
