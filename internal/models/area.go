c8c45c54e5748feca211990c7068e4a410bf94e1
Darth-Tenebros <yolisapingilili2@gmail.com>
2024-01-27 14:01:26 +0200

    typo

--- internal/models/area.go
+++ internal/models/area.go
@@ -2,7 +2,7 @@ package models
 
 type Area struct {
 	AreaName string `json:"area_name"`
-	Finish   string `json:"fnish"`
+	Finish   string `json:"finsh"`
 	Source   string `json:"source"`
 	Stage    int    `json:"stage"`
 	Start    string `json:"start"`
