d0a91cc709024973953a255c3b6770c5069aad56
Darth-Tenebros <yolisapingilili2@gmail.com>
2024-01-28 16:55:07 +0200

    list-view

---
+++ internal/ui/list_view.go
@@ -0,0 +1,35 @@
+package ui
+
+import (
+	"github.com/charmbracelet/bubbles/list"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+var docStyle = lipgloss.NewStyle().Margin(1, 2)
+
+type ListModel struct {
+	list         list.Model
+	SelectedItem int
+}
+
+func (m ListModel) Init() tea.Cmd {
+	return nil
+}
+
+func (m ListModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		}
+
+	}
+	m.list, cmd = m.list.Update(msg)
+	return m, cmd
+}
+func (m ListModel) View() string {
+	return docStyle.Render(m.list.View())
+}
