b028da999320321b71596aa4b155fe1397772e46
Darth-Tenebros <yolisapingilili2@gmail.com>
2024-01-31 23:41:18 +0200

    Area model repo for db interaction

---
+++ internal/repository/area_repo.go
@@ -0,0 +1,49 @@
+package repository
+
+import (
+	"database/sql"
+	"eskom-se-poes/internal/utils"
+	"fmt"
+)
+
+type AreaRepo struct {
+	DB *sql.DB
+}
+
+func NewAreaRepo(db *sql.DB) *AreaRepo {
+	return &AreaRepo{
+		DB: db,
+	}
+}
+func (area *AreaRepo) AddFavourite(areaName string) (string, error) {
+	id := utils.GenerateId(areaName)
+
+	exists, err := area.idExists(id)
+	if err != nil {
+		return "", err
+	}
+
+	if !exists {
+		sqlStatement, err := area.DB.Prepare("INSERT INTO projects(project_id, project_name) VALUES (?, ?)")
+		if err != nil {
+			return "", err
+		}
+		defer sqlStatement.Close()
+
+		_, err = sqlStatement.Exec(id, areaName)
+		if err != nil {
+			return "", err
+		}
+		return id, nil
+	}
+	return "", fmt.Errorf("the area is already in the favourites database")
+}
+
+func (area *AreaRepo) idExists(id string) (bool, error) {
+	var count int
+	err := area.DB.QueryRow("SELECT COUNT(*) FROM areas WHERE id = ?", id).Scan(&count)
+	if err != nil {
+		return false, err
+	}
+	return count > 0, nil
+}
