1e083d3e756ce6f09f7fcdd4152a725b4d25ffa5
Darth-Tenebros <yolisapingilili2@gmail.com>
2024-02-01 01:17:12 +0200

    logging

--- cmd/eskom-se-poes/main.go
+++ cmd/eskom-se-poes/main.go
@@ -4,10 +4,23 @@ import (
 	"eskom-se-poes/internal/ui"
 	"fmt"
 	tea "github.com/charmbracelet/bubbletea"
+	"log"
 	"os"
 )
 
+func initLogger() {
+	file, err := os.OpenFile("logfile.txt", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatal("Failed to open log file:", err)
+	}
+	defer file.Close()
+
+	log.SetOutput(file)
+}
+
 func main() {
+
+	initLogger()
 
 	listModel := ui.LoadListView()
 	p := tea.NewProgram(listModel, tea.WithAltScreen())
