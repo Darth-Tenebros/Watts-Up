28cdee660a39d371c49baec2a8934688917bf489
Darth-Tenebros <yolisapingilili2@gmail.com>
2024-01-31 23:42:44 +0200

    project-wide util functions

--- internal/utils/util.go
+++ internal/utils/util.go
@@ -1,75 +1,11 @@
 package utils
 
 import (
-	"fmt"
-	"github.com/charmbracelet/bubbles/table"
-	"github.com/charmbracelet/lipgloss"
-	"io"
-	"net/http"
+	"crypto/sha256"
+	"encoding/hex"
 )
 
-func LoadTableView(areaName string) table.Model {
-	link := "https://eskom-calendar-api.shuttleapp.rs/outages/"
-
-	schedule, err := getSchedule(link, areaName)
-	if err != nil {
-		fmt.Print(err)
-	}
-
-	var rows []table.Row
-	for _, outage := range schedule.Times {
-		rows = append(rows, outage.OutageToSlice())
-	}
-
-	columns := []table.Column{
-		{Title: "Stage", Width: 5},
-		{Title: "Area Name", Width: 30},
-		{Title: "Start", Width: 30},
-		{Title: "Finish", Width: 30},
-	}
-
-	t := table.New(
-		table.WithColumns(columns),
-		table.WithRows(rows),
-		table.WithFocused(true),
-		table.WithHeight(15),
-	)
-
-	s := table.DefaultStyles()
-	s.Header = s.Header.
-		BorderStyle(lipgloss.NormalBorder()).
-		BorderForeground(lipgloss.Color("240")).
-		BorderBottom(true).
-		Bold(false)
-	s.Selected = s.Selected.
-		Foreground(lipgloss.Color("229")).
-		Background(lipgloss.Color("57")).
-		Bold(false)
-	t.SetStyles(s)
-
-	//m := ui.TableModel{Table: t}
-	return t
-}
-
-func getSchedule(link, area string) (*Schedule, error) {
-	fullUrl := link + area
-	var schedule Schedule
-
-	res, err := http.Get(fullUrl)
-	if err != nil {
-		return nil, err
-	}
-	defer res.Body.Close()
-
-	body, err := io.ReadAll(res.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	err = schedule.UnmarshalResponse(body)
-	if err != nil {
-		return nil, err
-	}
-
-	return &schedule, nil
+func GenerateId(areaName string) string {
+	hash := sha256.Sum256([]byte(areaName))
+	return hex.EncodeToString(hash[:])
 }
