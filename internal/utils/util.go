8859017e966e107db703c16e84339d8165d30bf7
Darth-Tenebros <yolisapingilili2@gmail.com>
2024-02-01 01:19:36 +0200

    db connection func

--- internal/utils/util.go
+++ internal/utils/util.go
@@ -2,10 +2,20 @@ package utils
 
 import (
 	"crypto/sha256"
+	"database/sql"
 	"encoding/hex"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func GenerateId(areaName string) string {
 	hash := sha256.Sum256([]byte(areaName))
 	return hex.EncodeToString(hash[:])
+}
+
+func OpenDatabase() (*sql.DB, error) {
+	db, err := sql.Open("sqlite3", "./store/favourites")
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
 }
