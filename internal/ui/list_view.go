7024506737cc2cbdb87f90773bc07016c8386011
Darth-Tenebros <yolisapingilili2@gmail.com>
2024-02-02 16:22:05 +0200

    delete (d) functionality in th favourites view

--- internal/ui/list_view.go
+++ internal/ui/list_view.go
@@ -38,6 +38,7 @@ var docStyle = lipgloss.NewStyle().Margin(1, 2)
 type ListModel struct {
 	List         list.Model
 	SelectedItem int
+	Infavourites bool
 }
 
 func (m ListModel) Init() tea.Cmd {
@@ -54,7 +55,6 @@ func (m ListModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 		case "enter":
 			areaName := m.List.Items()[m.List.Index()]
-			//re := regexp.MustCompile("[^a-zA-Z0-9-]")
 			return TableModel{Table: LoadTableView(areaName.FilterValue())}, nil
 
 		case "a":
@@ -76,6 +76,9 @@ func (m ListModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			return m, nil
 
 		case "b":
+			if m.Infavourites {
+				m.Infavourites = false
+			}
 			listModel := LoadListView()
 			return listModel, nil
 
@@ -94,9 +97,40 @@ func (m ListModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			}
 			items := AreasToListItems(names)
 			m := ListModel{List: list.New(items, list.NewDefaultDelegate(), 10, 10)}
-			//m.List.SetHeight(35)
-			//m.List.SetWidth(50)
 			m.List.Title = "SA Load Shedding Areas - FAVOURITES"
+			m.Infavourites = true
+			return m, nil
+
+		case "d":
+			if !m.Infavourites {
+				return m, nil
+			}
+
+			// delete the area
+			db, err := utils.OpenDatabase()
+			if err != nil {
+				log.Println(err)
+				return m, nil
+			}
+			defer db.Close()
+			areaRepo := repository.NewAreaRepo(db)
+			id := m.List.Items()[m.List.Index()].FilterValue()
+			err = areaRepo.DeleteAreaFromFavourites(utils.GenerateId(id))
+			if err != nil {
+				log.Println(err)
+			}
+
+			// fetch current db data, re-render ui
+			// TODO: extract this to a util function
+			names, err := areaRepo.GetAllAreaNames()
+			if err != nil {
+				log.Println(err)
+				return m, nil
+			}
+			items := AreasToListItems(names)
+			m := ListModel{List: list.New(items, list.NewDefaultDelegate(), 10, 10)}
+			m.List.Title = "SA Load Shedding Areas - FAVOURITES"
+			m.Infavourites = true
 			return m, nil
 
 		}
